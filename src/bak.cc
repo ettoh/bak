cfe81e22bdd9e02d4791f9048c0b70ea5ab996ac
Hendrik <83767323+mc-thulu@users.noreply.github.com>
2024-04-15 17:45:50 +0800

    Refactoring

--- src/bak.cc
+++ src/bak.cc
@@ -27,7 +27,7 @@ string escapeRegex(const string &input) {
 // Looks for all previous backups of a given file in a given directory. A backup
 // is a file in the form "ORIGINAL_FILENAME-N.bak", where N is the backup ID.
 // The largest backup ID is returned.
-int latestBackupID(const fs::path dir, string original_filename) {
+int latestBackupID(const fs::path &dir, const string &original_filename) {
   // Find latest backup id.
   int latest_id = -1;  // later, +1 would yield 0
   for (auto const &file : fs::directory_iterator{dir}) {
@@ -43,10 +43,25 @@ int latestBackupID(const fs::path dir, string original_filename) {
   return latest_id;
 }
 
+// Destination for the backup.
+fs::path getBackupPath(const fs::path &original_file) {
+  // Extract base directory; use current directory if nothing else is specified.
+  fs::path dir = original_file.parent_path();
+  if (dir.empty()) {
+    dir = "./";
+  }
+
+  // new filename format: e.g. "directory/old_filename.oldtype-03.bak"
+  int latest_id = latestBackupID(dir, original_file.filename());
+  fs::path new_file = dir.append(original_file.filename().string() + "-" +
+                                 std::format("{:02}", latest_id + 1) + ".bak");
+  return new_file;
+}
+
 // Creates a backup of a file or directory. The backup is named
 // "ORIGINAL_FILENAME-N.bak", where N is the backup ID. The backup ID is
-// increased for each backup (that lives in the same folder). The new file is
-// stored in the same folder as the original file.
+// increased for each backup (that lives in the same folder). The new file
+// is stored in the same folder as the original file.
 void backup(const string &path) {
   // Later, path is split into directory and filename. If the user input ends
   // with '/', the filename would be empty. Therefore, remove an ending '/'.
@@ -62,19 +77,8 @@ void backup(const string &path) {
     return;
   }
 
-  // Extract base directory; use current directory if nothing else is specified.
-  fs::path dir = original_file.parent_path();
-  if (dir.empty()) {
-    dir = "./";
-  }
-
-  // Create filename for backup.
-  // e.g. "directory/old_filename.oldtype-03.bak"
-  int latest_id = latestBackupID(dir, original_file.filename());
-  fs::path new_file = dir.append(original_file.filename().string() + "-" +
-                                 std::format("{:02}", latest_id + 1) + ".bak");
-
   // Create backup.
+  fs::path new_file = getBackupPath(original_file);
   std::error_code copy_error;
   fs::copy(original_file, new_file, fs::copy_options::recursive, copy_error);
   if (copy_error) {
